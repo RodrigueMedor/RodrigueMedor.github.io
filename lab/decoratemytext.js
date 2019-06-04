7147b4691cbae3a5a9798eeefab7dfc08c5dbf8c
RodrigueMedor <rodriguemedor@yahoo.fr>
2019-06-03 23:45:26 -0500

    all character

---
+++ lab/decoratemytext.js
@@ -0,0 +1,25 @@
+alert("Hello, world!");
+
+window.onload = function () {
+    document.getElementById("biggerButton").onclick = textArea;
+    function textArea() {
+    document.getElementById("textarea").style.fontSize="49px";
+    }
+    }
+
+    function checkFluency()
+    {
+      var checkbox = document.getElementById('fluency');
+      if (checkbox.checked != true)
+      {
+        alert("you need to be fluent in English to apply for the job");
+      }
+    }
+    function checkFluency()
+{
+  var checkbox = document.getElementById('fluency');
+  if (checkbox.checked != true)
+  {
+    alert("you need to be fluent in English to apply for the job");
+  }
+}
