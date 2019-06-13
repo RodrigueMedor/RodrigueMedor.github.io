593cb6cf251f8c0f079b07b9f962afac83c933e4
medor <medor@medor-PC>
2019-06-12 23:42:16 -0500

    Assignment-11_Servlet_Intro

--- MyProject/src/main/java/com/medor/MyServlet.java
+++ MyProject/src/main/java/com/medor/MyServlet.java
@@ -19,15 +19,13 @@ public class MyServlet extends HttpServlet {
 		
 		PrintWriter pri = resp.getWriter();
 		
-		//System.out.println("*************************************************************************");
+		
 		pri.print("Thank you! ["+ name +"] for contacting us. We should receive reply from us with in 24 hrs in your email address ["+ email +"]. Let us know in our support email ["+ problem +"] if you don’t receive reply within 24 hrs. Please be sure to attach your reference ["+ description +"] in your email.\n" + 
 				"Support_email should come from context param.\n" + 
 				"Support_ticket_id is generated automatically for every request.");
 	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//resp.getWriter().println("<h1>Hello</h1>");
 		req.getRequestDispatcher("/index.jsp").forward(req, resp);
 	}
 
